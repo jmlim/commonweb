abd21d4ef6550eabff02693e45834186f95f0b9b
Lim Jeong muk <hackerljm@gmail.com>
2019-03-04 19:28:17 +0900

    스터디

---
+++ src/main/java/io/jmlim/commonweb/post/PostController.java
@@ -0,0 +1,41 @@
+package io.jmlim.commonweb.post;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class PostController {
+
+    private final PostRepository postRepository;
+
+    public PostController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    /*@GetMapping("/posts/{id}")
+    public String getPost(@PathVariable Long id) {
+        Optional<Post> byId = postRepository.findById(id);
+        Post post = byId.get();
+        return post.getTitle();
+    }*/
+
+    /**
+     * id 를 받아 Post 로 컨버팅 해줌. 위의 과정 생략이 가능.
+     * DomainClassConverter 가 자동으로 해준다.
+     */
+    @GetMapping("/posts/{id}")
+    public String getPost(@PathVariable("id") Post post) {
+        return post.getTitle();
+    }
+
+    @GetMapping("/posts")
+    public Page<Post> getPosts(Pageable pageable) {
+        return postRepository.findAll(pageable);
+    }
+}
+
---
+++ src/test/java/io/jmlim/commonweb/post/PostControllerTest.java
@@ -0,0 +1,59 @@
+package io.jmlim.commonweb.post;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+/**
+ * integration test
+ * 모든 빈이 전부 올라온 상태로 테스트를 질행.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    public void getPost() throws Exception {
+        Post post = new Post();
+        post.setTitle("jpa");
+        postRepository.save(post);
+
+        mockMvc.perform(get("/posts/" + post.getId()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("jpa"));
+    }
+
+    @Test
+    public void getPosts() throws Exception {
+        Post post = new Post();
+        post.setTitle("jpa");
+        postRepository.save(post);
+
+        mockMvc.perform(get("/posts/")
+                .param("page", "0")
+                .param("size", "10")
+                .param("sort", "created,desc")
+                .param("sort", "title"))
+                .andDo(print())
+                .andExpect(status().isOk());
+        // .andExpect(jsonPath("$.content[0].title", is("jpa"))); ????
+        // TODO:  이거 왜 못받는지 확인해볼것..
+    }
+}
