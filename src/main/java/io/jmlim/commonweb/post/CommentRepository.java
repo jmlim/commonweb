743d67a2c66754cb206c1f552bb4d3161a58d306
Lim Jeong muk <hackerljm@gmail.com>
2019-03-05 18:07:00 +0900

    스프링데이터 JPA 트랜잭션 강의듣고 올리기

--- src/main/java/io/jmlim/commonweb/post/CommentRepository.java
+++ src/main/java/io/jmlim/commonweb/post/CommentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,5 +31,9 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpec
     List<CommentSummary> findByPost_Id(Long id);
 
     //  프로젝션을 적용할 클래스가 각각 다를 경우 오버라이딩이 안되므로.. 이렇게 사용, 단점은 타입을 지정해야함.
+    // Transactional을 readOnly = true 로 주게되면 장점이 flush 모드를 never로 설정하여,
+    // dirty checking을 하지 않도록 한다.
+    // 데이터를 많이 가져오는 경우에 Dirty checking 를 꺼주면 성능이 향상됨
+    @Transactional(readOnly = true)
     <T> List<T> findByPost_Id(Long id, Class<T> type);
 }
